c2bdce473adbd68fd4d305d2129b175d9dd5161d
GyuBa <sansam1010@naver.com>
2024-11-12 22:54:27 +0900

    First commit

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    int8_t opt;
+
+    while((opt = getopt(argc, argv, "vfi")) != -1) {
+        switch (opt) {
+            case 'v'
+
+            case 'f'
+
+            case 'i'
+        }
+    }
+}
